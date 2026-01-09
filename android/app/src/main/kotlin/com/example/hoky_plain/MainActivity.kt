997a84ebb6b44cf618a28a4e0674cb7ddf11da4b
qahtanm <64630276+qahtanm@users.noreply.github.com>
2026-01-09 18:53:08 +0300

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/hoky_plain/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hoky_plain
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "F:\\fluatr\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "F:\\app-12\\hoky_plain" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=F:\\fluatr\\flutter"
+  "PROJECT_DIR=F:\\app-12\\hoky_plain"
+  "FLUTTER_ROOT=F:\\fluatr\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=F:\\app-12\\hoky_plain\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=F:\\app-12\\hoky_plain"
+  "FLUTTER_TARGET=F:\\app-12\\hoky_plain\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzguNg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049OGI4NzI4Njg0OQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NzhmYzMwMTJlNA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC43"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=F:\\app-12\\hoky_plain\\.dart_tool\\package_config.json"
+)
